989f5ade6406dc01b9847fb88b929afd7cb8436a
ClaudiaDanciu <contact@claudiadanciu.com>
2022-11-15 09:08:12 +0000

    Scaffold student pages

--- ClaudiaProject/Data/ApplicationDbContext.cs
+++ ClaudiaProject/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ClaudiaProject.Models;
 
 namespace ClaudiaProject.Data
 {
@@ -9,5 +10,6 @@ namespace ClaudiaProject.Data
             : base(options)
         {
         }
+        public DbSet<ClaudiaProject.Models.Student> Student { get; set; }
     }
 }
---
+++ ClaudiaProject/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ClaudiaProject.Data;
+using ClaudiaProject.Models;
+
+namespace ClaudiaProject.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ClaudiaProject.Data.ApplicationDbContext _context;
+
+        public IndexModel(ClaudiaProject.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Student != null)
+            {
+                Student = await _context.Student.ToListAsync();
+            }
+        }
+    }
+}
