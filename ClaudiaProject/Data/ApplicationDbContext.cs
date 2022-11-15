00f9bcc94e7d01411c1b11cc03cecb91190790f5
ClaudiaDanciu <contact@claudiadanciu.com>
2022-11-15 10:15:52 +0000

    Add db

--- ClaudiaProject/Data/ApplicationDbContext.cs
+++ ClaudiaProject/Data/ApplicationDbContext.cs
@@ -10,6 +10,15 @@ namespace ClaudiaProject.Data
             : base(options)
         {
         }
-        public DbSet<ClaudiaProject.Models.Student> Student { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
     }
 }
